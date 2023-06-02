9536c33ef5fb9ba091fa1e6e78056027ca0e0c9d
THIORA <135319922+THIORA@users.noreply.github.com>
2023-06-02 16:07:33 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,19 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====================================== RESTART: C:/Users/ADMIN/Desktop/THIORA/1.py =====================================
+Hello, World!
+>>> 
+====================================== RESTART: C:/Users/ADMIN/Desktop/THIORA/1.py =====================================
+Hello, World!
+what is your name?THIORA
+Hello THIORA
+>>> age=12
+>>> print(age)
+12
+>>> weight=29
+>>> print(weight)
+29
+>>> height=144
+>>> print(height)
+144
